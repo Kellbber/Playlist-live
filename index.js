ed11861e638e2d27cd187c83656bccd77f84c640
Kellbber <kellbberbarcarolo@gmail.com>
2022-02-03 20:52:49 -0300

    k

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
 const express = require('express')
 const connetToDb = require('./database/db');
-
-
-
-
+const path = require('path');
 
 const app = express()
 
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+
+
 connetToDb();
 
 app.get('/hello', (req, res) => {
-  res.send('Hello World')
+  res.render('index')
 });
 
 app.listen(port, () => console.log(`Servidor rodando em htttp://localhost:${port}`));
